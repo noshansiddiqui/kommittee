2de0c4666bd80dc8d73067c78d82f0caa6fb250d
User <user@example.com>
2026-06-11 13:19:39 +0100

    fix: use absolute paths for PWA install on GitHub Pages

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE = 'kommittee-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json']))));
+const CACHE = 'kommittee-v2';
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/kommittee/', '/kommittee/index.html', '/kommittee/manifest.json']))); });
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
